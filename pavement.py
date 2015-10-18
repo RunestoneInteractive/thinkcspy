8cb39c57beaafebe77b3c81c027324a01c95e2b3
Brad Miller <bonelake@mac.com>
2015-10-18 18:42:18 -0500

    fix typo

--- pavement.py
+++ pavement.py
@@ -42,6 +42,7 @@ options(
             'course_url':master_url,
             'use_services': 'true',
             'python3': 'true',
+            'dburl': 'postgresql://bmiller@localhost/runestone',
         }
 
     )
